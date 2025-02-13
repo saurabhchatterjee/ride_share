11c7cfb8594a4d89a89d3bec80d9be51828f596c
Saurabh Chatterjee <schatterjee60@gatech.edu>
2025-02-13 11:25:18 -0600

    skeleton phase 1

---
+++ app/src/main/java/com/rideshare/app/AppApplication.java
@@ -0,0 +1,13 @@
+package com.rideshare.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppApplication.class, args);
+	}
+
+}
